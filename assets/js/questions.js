204986f87db2cb748a121ee3846e134eb9ed9e41
Trillogy <trillogy@Gabes-Mac-mini.local>
2021-06-16 18:05:37 -0400

    final

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+  {
+    ques: "What is your instructors name?",
+    choices: ["Mike", "Bob", "Gabe"],
+    answer: "Gabe",
+  },
+  {
+    ques: "What is your SSM's name?",
+    choices: ["Dana", "Susan", "Stephanie"],
+    answer: "Stephanie",
+  },
+  {
+    ques: "What is one of your TA's name?",
+    choices: ["Joe", "Taleh", "Gabe"],
+    answer: "Taleh",
+  },
+  {
+    ques: "What is one of your TA's name?",
+    choices: ["Bob", "TA", "Lawrence"],
+    answer: "Lawrence",
+  },
+  {
+    ques: "What is the name of this college?",
+    choices: ["UMD", "UDC", "GWU"],
+    answer: "GWU",
+  },
+];
